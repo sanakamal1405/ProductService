2e69debe1d95c47de839496d2448b0df14ef9feb
Sana Kamal <sanakamal1405@gmail.com>
2024-06-23 16:23:54 +0530

    Added Eureka-Client config

--- src/main/java/com/microservice/productservice/config/RestTemplateConfig.java
+++ src/main/java/com/microservice/productservice/config/RestTemplateConfig.java
@@ -2,14 +2,17 @@ package com.microservice.productservice.config;
 
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplateBuilder().build();
     }
